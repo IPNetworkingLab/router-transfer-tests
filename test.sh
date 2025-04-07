7023765b6110aebcd39d8cc9a7632774035c6556
Matthieu Baerts <matthieu.baerts@uclouvain.be>
2025-04-07 14:57:40 +0200

    topo: add IPv6 addresses
    For the moment, the same way as with IPv4.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@uclouvain.be>

--- test.sh
+++ test.sh
@@ -17,56 +17,100 @@ cleanup()
 
 trap cleanup EXIT
 
+# $1: ns ; $2: dev ; $3: ip-pre ; $4: ip-suff
+ip_addr_del()
+{
+	ip -n "${1}" addr del dev "${2}" "10.0.${3}.${4}/24"
+	# ip -n "${1}" addr del dev "${2}" "dead:beef:${3}::${4}/64" ## removed when the iface is down
+}
+
+# $1: ns ; $2: dev ; $3: ip-pre ; $4: ip-suff
+ip_addr_add()
+{
+	ip -n "${1}" addr add dev "${2}" "10.0.${3}.${4}/24"
+	ip -n "${1}" addr add dev "${2}" "dead:beef:${3}::${4}/64" nodad
+}
+
+# $1: ns ; $2: dev ; $3: ip-pre ; $4: ip-suff ; $5: metric
+ip_route_add_default()
+{
+	ip -n "${1}" route add dev "${2}" default via "10.0.${3}.${4}" metric "${5}"
+	ip -n "${1}" route add dev "${2}" default via "dead:beef:${3}::${4}" metric "${5}"
+}
+
+# $1: ns ; $2: dev ; $3: ip-pre ; $4: ip-suff ; $5: ip-via-pre ; $6: ip-via-suff
+ip_route_add_prefix()
+{
+	ip -n "${1}" route add dev "${2}" "10.0.${3}.${4}/24" via "10.0.${5}.${6}"
+	ip -n "${1}" route add dev "${2}" "dead:beef:${3}::${4}/64" via "dead:beef:${5}::${6}"
+
+}
+
+# $1: ns ; $2: dev ; $3: ip-pre ; $4: ip-suff ; $5: id ; $6: flags
+ip_mptcp_add()
+{
+	ip -n "${1}" mptcp endpoint add "10.0.${3}.${4}" dev "${2}" id "${5}" "${@:6}"
+	ip -n "${1}" mptcp endpoint add "dead:beef:${3}::${4}" dev "${2}" id "$((${5} * 10))" "${@:6}"
+}
+
+# $1: ns ; $2: dev ; $3: id
+ip_mptcp_del()
+{
+	ip -n "${1}" mptcp endpoint del id "${2}"
+	ip -n "${1}" mptcp endpoint del id "$((${2} * 10))"
+}
+
 cpe_switch_off()
 {
 	ip -n "${NS}_cpe" link set dev "pho" down
 	ip -n "${NS}_cpe" link set dev "cli" down
 	ip -n "${NS}_pho" link set "cpe" down
-	ip -n "${NS}_pho" addr del dev "cpe" 10.0.0.2/24
-	ip -n "${NS}_pho" mptcp endpoint del id 1
+	ip_addr_del "${NS}_pho" cpe 0 2
+	ip_mptcp_del "${NS}_pho" 1
 	ip -n "${NS}_cli" link set "cpe" down
-	ip -n "${NS}_cli" addr del dev "cpe" 10.0.0.3/24
-	ip -n "${NS}_cli" mptcp endpoint del id 1
+	ip_addr_del "${NS}_cli" cpe 0 3
+	ip_mptcp_del "${NS}_cli" 1
 
 	ip -n "${NS}_pho" link set "cli" up
-	ip -n "${NS}_pho" addr add dev "cli" 10.0.1.1/24
+	ip_addr_add "${NS}_pho" cli 1 1
 
 	ip -n "${NS}_cli" link set "pho" up
-	ip -n "${NS}_cli" addr add dev "pho" 10.0.1.2/24
-	ip -n "${NS}_cli" route add default via 10.0.1.1 dev "pho" metric 200
+	ip_addr_add "${NS}_cli" pho 1 2
+	ip_route_add_default "${NS}_cli" pho 1 1 200
 	sleep .1 # making sure the route is ready
-	ip -n "${NS}_cli" mptcp endpoint add 10.0.1.2 dev "pho" id 2 subflow
+	ip_mptcp_add "${NS}_cli" pho 1 2 2 subflow
 }
 
 cpe_switch_on()
 {
 	ip -n "${NS}_pho" link set "cli" down
-	ip -n "${NS}_pho" addr del dev "cli" 10.0.1.1/24
+	ip_addr_del "${NS}_pho" cli 1 1
 	ip -n "${NS}_cli" link set "pho" down
-	ip -n "${NS}_cli" addr del dev "pho" 10.0.1.2/24
-	ip -n "${NS}_cli" mptcp endpoint del id 2
+	ip_addr_del "${NS}_cli" pho 1 2
+	ip_mptcp_del "${NS}_cli" 2
 
 	ip -n "${NS}_cpe" link set dev "pho" up
 	ip -n "${NS}_cpe" link set dev "cli" up
 
 	ip -n "${NS}_pho" link set "cpe" up
-	ip -n "${NS}_pho" addr add dev "cpe" 10.0.0.2/24
-	ip -n "${NS}_pho" route add default via 10.0.0.1 dev "cpe" metric 100
+	ip_addr_add "${NS}_pho" cpe 0 2
+	ip_route_add_default "${NS}_pho" cpe 0 1 100
 	sleep .1 # making sure the route is ready
-	ip -n "${NS}_pho" mptcp endpoint add 10.0.0.2 dev "cpe" id 1 subflow
+	ip_mptcp_add "${NS}_pho" cpe 0 2 1 subflow
 
 	ip -n "${NS}_cli" link set "cpe" up
-	ip -n "${NS}_cli" addr add dev "cpe" 10.0.0.3/24
-	ip -n "${NS}_cli" route add default via 10.0.0.1 dev "cpe" metric 100
+	ip_addr_add "${NS}_cli" cpe 0 3
+	ip_route_add_default "${NS}_cli" cpe 0 1 100
 	sleep .1 # making sure the route is ready
-	ip -n "${NS}_cli" mptcp endpoint add 10.0.0.3 dev "cpe" id 1 subflow
+	ip_mptcp_add "${NS}_cli" cpe 0 3 1 subflow
 }
 
+# $1: IP server
 iperf_test()
 {
 	ip netns exec "${NS}_srv" mptcpize run iperf3 -s -D
 	sleep .1 # making sure the daemon is launched
-	ip netns exec "${NS}_cli" mptcpize run iperf3 -c 10.0.4.2 -t 999 -i 0 &
+	ip netns exec "${NS}_cli" mptcpize run iperf3 -c "${1}" -t 999 -i 0 &
 	ip netns exec "${NS}_cli" ifstat -b -i cpe,pho &
 	for _ in $(seq 4); do
 		sleep 5
@@ -85,6 +129,8 @@ setup()
 		local ns="${NS}_${suffix}"
 		ip netns add "${ns}"
 		ip -n "${ns}" link set lo up
+		ip netns exec "${ns}" sysctl -wq net.ipv4.ip_forward=1
+		ip netns exec "${ns}" sysctl -wq net.ipv6.conf.all.forwarding=1
 	done
 
 	#        .3.2
@@ -104,48 +150,48 @@ setup()
 	ip link add "srv" netns "${NS}_net" type veth peer name "net" netns "${NS}_srv"
 
 	ip -n "${NS}_pho" link set "cpe" up
-	ip -n "${NS}_pho" addr add dev "cpe" 10.0.0.2/24
-	ip -n "${NS}_pho" route add default via 10.0.0.1 dev "cpe" metric 100
-	ip -n "${NS}_pho" mptcp endpoint add 10.0.0.2 dev "cpe" id 1 subflow
+	ip_addr_add "${NS}_pho" cpe 0 2
+	ip_route_add_default "${NS}_pho" cpe 0 1 100
+	ip_mptcp_add "${NS}_pho" cpe 0 2 1 subflow
 	ip -n "${NS}_pho" link set "net" up
-	ip -n "${NS}_pho" addr add dev "net" 10.0.3.2/24
-	ip -n "${NS}_pho" route add default via 10.0.3.1 dev "net" metric 200
-	ip -n "${NS}_pho" mptcp endpoint add 10.0.3.2 dev "net" id 2 subflow backup
+	ip_addr_add "${NS}_pho" net 3 2
+	ip_route_add_default "${NS}_pho" net 3 1 200
+	ip_mptcp_add "${NS}_pho" net 3 2 2 subflow backup
 	tc -n "${NS}_pho" qdisc add dev "net" root netem rate 20mbit delay 10ms
 
 	ip -n "${NS}_cli" link set "cpe" up
-	ip -n "${NS}_cli" addr add dev "cpe" 10.0.0.3/24
-	ip -n "${NS}_cli" route add default via 10.0.0.1 dev "cpe" metric 100
-	ip -n "${NS}_cli" mptcp endpoint add 10.0.0.3 dev "cpe" id 1 subflow
+	ip_addr_add "${NS}_cli" cpe 0 3
+	ip_route_add_default "${NS}_cli" cpe 0 1 100
+	ip_mptcp_add "${NS}_cli" cpe 0 3 1 subflow
 
 	ip -n "${NS}_cpe" link add name "br" type bridge
-	ip -n "${NS}_cpe" addr add dev "br" 10.0.0.1/24
+	ip_addr_add "${NS}_cpe" br 0 1
 	ip -n "${NS}_cpe" link set dev "br" up
 	ip -n "${NS}_cpe" link set dev "pho" master "br"
 	ip -n "${NS}_cpe" link set dev "cli" master "br"
 	ip -n "${NS}_cpe" link set dev "pho" up
 	ip -n "${NS}_cpe" link set dev "cli" up
 	ip -n "${NS}_cpe" link set "net" up
-	ip -n "${NS}_cpe" addr add dev "net" 10.0.2.2/24
-	ip -n "${NS}_cpe" route add default via 10.0.2.1 dev "net" metric 100
+	ip_addr_add "${NS}_cpe" net 2 2
+	ip_route_add_default "${NS}_cpe" net 2 1 100
 	tc -n "${NS}_cpe" qdisc add dev "net" root netem rate 40mbit delay 5ms
 
 	ip -n "${NS}_net" link set "pho" up
-	ip -n "${NS}_net" addr add dev "pho" 10.0.3.1/24
+	ip_addr_add "${NS}_net" pho 3 1
 	tc -n "${NS}_net" qdisc add dev "pho" root netem rate 20mbit delay 10ms
 	ip -n "${NS}_net" link set "cpe" up
-	ip -n "${NS}_net" addr add dev "cpe" 10.0.2.1/24
+	ip_addr_add "${NS}_net" cpe 2 1
 	tc -n "${NS}_net" qdisc add dev "cpe" root netem rate 40mbit delay 5ms
 	ip -n "${NS}_net" link set "srv" up
-	ip -n "${NS}_net" addr add dev "srv" 10.0.4.1/24
-	ip -n "${NS}_net" route add default via 10.0.4.2 dev "srv" metric 100
+	ip_addr_add "${NS}_net" srv 4 1
+	ip_route_add_default "${NS}_net" srv 4 2 100
 	# not to have to deal with NATs
-	ip -n "${NS}_net" route add 10.0.0.0/24 via 10.0.2.2 dev "cpe"
-	ip -n "${NS}_net" route add 10.0.1.0/24 via 10.0.3.2 dev "pho"
+	ip_route_add_prefix "${NS}_net" cpe 0 0 2 2
+	ip_route_add_prefix "${NS}_net" pho 1 0 3 2
 
 	ip -n "${NS}_srv" link set "net" up
-	ip -n "${NS}_srv" addr add dev "net" 10.0.4.2/24
-	ip -n "${NS}_srv" route add default via 10.0.4.1 dev "net" metric 100
+	ip_addr_add "${NS}_srv" net 4 2
+	ip_route_add_default "${NS}_srv" net 4 1 100
 	ip -n "${NS}_srv" mptcp limits set subflows 8
 }
 
@@ -153,7 +199,10 @@ setup
 
 case "${1}" in
 	"auto")
-		iperf_test
+		iperf_test 10.0.4.2
+		;;
+	"auto-v6")
+		iperf_test dead:beef:4::2
 		;;
 	*)
 		export -f cpe_switch_on cpe_switch_off iperf_test
